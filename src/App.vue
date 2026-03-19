a4211c46ad718623775e83d69f8e47d96a5ce57a
Abdulrasaq Mustapha <razicruz@gmail.com>
2026-03-19 03:32:48 +0100

    feat: add app files

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AppMain from './components/AppMain.vue'
+import AppSidebar from './components/AppSidebar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen xl:grid xl:grid-cols-[267px_1fr]">
+    <AppSidebar />
+    <AppMain />
+  </div>
+</template>
